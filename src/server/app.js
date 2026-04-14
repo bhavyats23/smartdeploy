9415b4c90efbc700244821a367a26f2ebb82077e
Bhavya <bhavyats23@gmail.com>
2026-04-14 16:08:00 +0530

    Fix: use app.use instead of app.get for catch-all route

--- src/server/app.js
+++ src/server/app.js
@@ -154,7 +154,7 @@ app.post("/api/deploy", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../dist")));
-  app.get("(.*)", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../../dist/index.html"));
   });
 }
